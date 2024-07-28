807beb62b46aaf14768bdf38dc6c3c25120ec0af
Shaneal Findley <37088454+sfindley899@users.noreply.github.com>
2024-07-27 22:11:04 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -29,3 +29,60 @@ async function initCovidGenderData() {
 async function initCovidRegionalData() {
 	return d3.csv(weekly_regional_cases_deaths_csv);
 }
+
+function renderCurrentTrendsGraph() {
+    // D3 code to render the current trends graph in the element with id 'current-trends-graph'
+}
+
+async function createGenderD3Chart() {
+	const margin = { top: 30, right: 20, bottom: 70, left: 50 },
+		width = 600 - margin.left - margin.right,
+		height = 300 - margin.top - margin.bottom;
+
+	const parseDate = d3.timeParse("%Y-%m");
+
+	const x = d3.scaleBand().range([0, width]).padding(0.1);
+	const y = d3.scaleLinear().range([height, 0]);
+
+	const svg = d3.select("#gender-chart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform",
+			"translate(" + margin.left + "," + margin.top + ")");
+
+	initCovidGenderData().then(data => {
+		data.forEach(d => {
+			d.date = parseDate(d.Year + "-" + d.Month);
+			d["COVID-19 Deaths"] = +d["COVID-19 Deaths"];
+		});
+
+		x.domain(data.map(d => d.date));
+		y.domain([0, d3.max(data, d => d["COVID-19 Deaths"])]);
+
+		svg.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x).tickFormat(d3.timeFormat("%b %Y")))
+			.selectAll("text")
+			.style("text-anchor", "end")
+			.attr("dx", "-.8em")
+			.attr("dy", "-.55em")
+			.attr("transform", "rotate(-90)");
+
+		svg.append("g")
+			.call(d3.axisLeft(y));
+
+		svg.selectAll("bar")
+			.data(data)
+			.enter().append("rect")
+			.style("fill", "steelblue")
+			.attr("x", d => x(d.date))
+			.attr("width", x.bandwidth())
+			.attr("y", d => y(d["COVID-19 Deaths"]))
+			.attr("height", d => height - y(d["COVID-19 Deaths"]));
+	});
+}
+
+async function loadGenderD3Chart() {
+	createGenderD3Chart();
+}
