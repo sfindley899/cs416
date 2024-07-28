e716ab8a55ef72dfb1fe90ab54d2a234df3808cb
Shaneal Findley <37088454+sfindley899@users.noreply.github.com>
2024-07-28 14:21:15 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+const body = document.body;
+const html = document.documentElement;
+const height = Math.max(
+	html.clientHeight,
+	html.scrollHeight,
+	html.offsetHeight
+);
+
 var monthly_deaths_csv = "ages_monthly_deaths.csv";
 var demographics_summary_csv = "race_total_outcomes.csv";
 var monthly_age_states_deaths_csv = "pages_states_monthly_dates.csv";
 var monthly_gender_dates_csv = "gender_monthly_dates.csv";
 var weekly_regional_cases_deaths_csv = "weekly_cases_and_deaths_per_state_region.csv";
+var cumulative_deaths_csv = "cumulative_deaths_us.csv";
 
 // Test
 function pageLoadTest()
@@ -28,6 +37,10 @@ async function initCovidGenderData() {
 
 async function initCovidRegionalData() {
 	return d3.csv(weekly_regional_cases_deaths_csv);
+}
+
+async function initCovidTotalDeathData() {
+	return d3.csv(cumulative_deaths_csv);
 }
 
 function renderCurrentTrendsGraph() {
@@ -83,6 +96,185 @@ async function createGenderD3Chart() {
 	});
 }
 
+async function createDeathsWeeklyChart() {
+	const margin = { top: 30, right: 20, bottom: 70, left: 50 },
+		width = 400 - margin.left - margin.right,
+		height = 300 - margin.top - margin.bottom;
+
+	const parseDate = d3.timeParse("%m/%d/%Y");
+
+	const x = d3.scaleTime().range([0, width]);
+	const y = d3.scaleLinear().range([height, 0]);
+
+	const svg = d3.select("#deaths-chart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform",
+			"translate(" + margin.left + "," + margin.top + ")");
+
+	initCovidRegionalData().then(data => {
+		data.forEach(d => {
+			d.start_date = parseDate(d.start_date);
+			d.new_deaths = +d.new_deaths;
+		});
+
+		x.domain(d3.extent(data, d => d.start_date));
+		y.domain([0, d3.max(data, d => d.new_deaths)]);
+
+		svg.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x));
+
+		svg.append("g")
+			.call(d3.axisLeft(y));
+
+		const line = d3.line()
+			.x(d => x(d.start_date))
+			.y(d => y(d.new_deaths));
+
+		svg.append("path")
+			.data([data])
+			.attr("class", "line")
+			.attr("d", line)
+			.style("fill", "none")
+			.style("stroke", "red")
+			.style("stroke-width", "2px");
+	});
+}
+
+async function createCasesWeeklyChart() {
+	const margin = { top: 30, right: 20, bottom: 70, left: 50 },
+		width = 400 - margin.left - margin.right,
+		height = 300 - margin.top - margin.bottom;
+
+	const parseDate = d3.timeParse("%m/%d/%Y");
+
+	const x = d3.scaleTime().range([0, width]);
+	const y = d3.scaleLinear().range([height, 0]);
+
+	const svg = d3.select("#cases-chart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform",
+			"translate(" + margin.left + "," + margin.top + ")");
+
+	initCovidRegionalData().then(data => {
+		data.forEach(d => {
+			d.date_updated = parseDate(d.date_updated);
+			d.new_cases = +d.new_cases;
+		});
+
+		x.domain(d3.extent(data, d => d.date_updated));
+		y.domain([0, d3.max(data, d => d.new_cases)]);
+
+		svg.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x));
+
+		svg.append("g")
+			.call(d3.axisLeft(y));
+
+		const line = d3.line()
+			.x(d => x(d.date_updated))
+			.y(d => y(d.new_cases));
+
+		svg.append("path")
+			.data([data])
+			.attr("class", "line")
+			.attr("d", line)
+			.style("fill", "none")
+			.style("stroke", "steelblue")
+			.style("stroke-width", "2px");
+	});
+}
+
+async function createCumulativeDeathsChart() {
+	const margin = { top: 30, right: 20, bottom: 70, left: 50 },
+		width = document.body.clientWidth - margin.left - margin.right,
+		height = 500 - margin.top - margin.bottom;
+
+	const parseDate = d3.timeParse("%m/%d/%Y");
+
+	const x = d3.scaleTime().range([0, width]);
+	const y = d3.scaleLinear().range([height, 0]);
+
+	const svg = d3.select("#total-deaths-chart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	initCovidTotalDeathData().then(data => {
+		data.forEach(d => {
+			d.date_value = parseDate(d.date_value);
+			d["Cumulative Deaths"] = +d["Cumulative Deaths"];
+		});
+
+		x.domain(d3.extent(data, d => d.date_value));
+		y.domain([0, d3.max(data, d => d["Cumulative Deaths"])]);
+
+		svg.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x));
+
+		svg.append("g")
+			.call(d3.axisLeft(y));
+
+		const line = d3.line()
+			.x(d => x(d.date_value))
+			.y(d => y(d["Cumulative Deaths"]));
+
+		svg.append("path")
+			.data([data])
+			.attr("class", "line")
+			.attr("d", line)
+			.style("fill", "none")
+			.style("stroke", "steelblue")
+			.style("stroke-width", "2px");
+
+		// Adding annotations
+		const annotations = [
+			{date: "1/11/2020", value: 1, text: "First Death"},
+			{date: "1/25/2020", value: 6, text: "6 Deaths"},
+			{date: "2/8/2020", value: 10, text: "10 Deaths"},
+			{date: "2/15/2020", value: 16, text: "16 Deaths"},
+			{date: "2/22/2020", value: 22, text: "22 Deaths"}
+		];
+
+		annotations.forEach(ann => {
+			const date = parseDate(ann.date);
+			svg.append("line")
+				.attr("x1", x(date))
+				.attr("x2", x(date))
+				.attr("y1", y(ann.value))
+				.attr("y2", height)
+				.attr("stroke", "red")
+				.attr("stroke-dasharray", "2,2");
+
+			svg.append("text")
+				.attr("x", x(date))
+				.attr("y", y(ann.value) - 10)
+				.attr("text-anchor", "middle")
+				.attr("class", "annotation")
+				.text(ann.text);
+		});
+	});
+}
+
 async function loadGenderD3Chart() {
 	createGenderD3Chart();
+}
+
+async function loadDeathsWeeklyD3Chart() {
+	createDeathsWeeklyChart();
+}
+
+async function loadCasesWeeklyD3Chart() {
+	createCasesWeeklyChart();
+}
+
+async function totalDeathsD3Chart() {
+	createCumulativeDeathsChart();
 }
